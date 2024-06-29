ad5cb2ce72afb7daaa11c5b83ac1256cb6e893bc
ArthurSenpai <arthursenpai@ArthurSenpais-MacBook-Pro.local>
2024-06-30 01:20:24 +0530

    handle rating system

--- src/components/ratings.tsx
+++ src/components/ratings.tsx
@@ -5,8 +5,22 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "./ui/tooltip";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
-const Ratings = () => {
+type pageProps ={
+  modelName?: string,
+}
+
+type handleRating = {
+  level: string,
+  rating: number
+}
+
+const Ratings = ({modelName}:pageProps) => {
+  const [handleClick, setHandleClick] = useState(false)
+  const [selectedRating, setSelectedRating] = useState(0);
+
   const biasLevels = [
     { level: "Not Biased", rating: 1 },
     { level: "Barely Biased", rating: 2 },
@@ -20,16 +34,33 @@ const Ratings = () => {
     { level: "Completely Biased", rating: 10 },
   ];
 
+  const handleRating = (ratingData:handleRating) =>{
+    const requestBody = {
+      ratingName: ratingData?.level,
+      rating: ratingData?.rating,
+      modelName: modelName,
+    }
+    setHandleClick(true)
+    setSelectedRating(ratingData.rating);
+    console.log(requestBody);
+  }
+
+  const handleRest = () =>{
+    setHandleClick(false)
+    setSelectedRating(0)
+  }
+
   return (
     <>
       <div className="flex flex-col justify-center items-start gap-5 pl-5 h-full mb-10 md:mb-0">
         <Label className="text-xl">Rate the Biasness:</Label>
-        <ul className="flex gap-2 items-center flex-wrap justify-center -space-x-px h-10 text-base">
+       <div className="flex gap-5 justify-around items-center flex-col md:flex-row  h-full">
+       <ul className="flex gap-2 items-center flex-wrap justify-center -space-x-px h-10 text-base">
           {biasLevels?.map((item) => (
-            <TooltipProvider delayDuration={100}>
+            <TooltipProvider delayDuration={100} key={`${item?.level} : ${item?.rating}`}>
               <Tooltip>
                 <TooltipTrigger>
-                  <li className="flex items-center justify-center px-4 h-10 leading-tight text-gray-50 rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer">
+                  <li className={`flex items-center justify-center px-4 h-10 leading-tight rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${selectedRating ? (selectedRating === item.rating ? "bg-white text-black" : "opacity-10 cursor-not-allowed text-white") : "bg-transparent"} dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer`}onClick={() => !handleClick ? handleRating(item) : null}>
                     {item?.rating}
                   </li>
                 </TooltipTrigger>
@@ -38,6 +69,10 @@ const Ratings = () => {
             </TooltipProvider>
           ))}
         </ul>
+        <Button className="bg-transparent border-2 border-white mt-20 md:mt-0" onClick={()=> handleRest()}>
+          Rate Again
+        </Button>
+       </div>
       </div>
     </>
   );
--- src/wrappers/Chat/ModelWrapper.tsx
+++ src/wrappers/Chat/ModelWrapper.tsx
@@ -9,14 +9,6 @@ type pageProps = {
 const ModelWrapper = ({ modelName, response }: pageProps) => {
   return (
     <div className="flex flex-col justify-center h-full items-start gap-5 py-4">
-      {/* <div className="lg:w-[700px] lg:h-[480px] w-[100%] h-full border-2 rounded-lg border-white relative">
-        <div className="absolute top-0 left-0 border-2 rounded-br-lg border-white w-36 h-10 text-center flex justify-center items-center">
-          {modelName}
-        </div>
-        <p className="absolute top-5 p-8 text-xl h-[180px] overflow-y-scroll ">
-          {response}
-        </p>
-      </div> */}
       <Card className="lg:w-[700px] lg:h-[480px] w-[100%] h-full p-4 bg-transparent text-white">
         <CardHeader className="text-4xl">
           {modelName}
@@ -25,7 +17,7 @@ const ModelWrapper = ({ modelName, response }: pageProps) => {
           {response}
         </CardDescription>
       </Card>
-      <Ratings />
+      <Ratings modelName={modelName} />
     </div>
   );
 };
