78d32e06491943046436d2aed2acf5bb8aaa6ae3
ArthurSenpai <arthursenpai@ArthurSenpais-MacBook-Pro.local>
2024-06-29 13:47:04 +0530

    made site responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-
-import Page from "./wrappers/Page"
-
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter, useLocation, Outlet } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Chat from "./pages/Chat.tsx";
+import Page from "./wrappers/Page";
 
-const routes = createBrowserRouter([{
-  path: "/",
-  element: <Home />,
-}, {
-  path: "/chat",
-  element: <Chat />
-}])
+const Layout = () => {
+  const location = useLocation();
+  return (
+    <Page page={location.pathname === "/" ? "home" : "chat"}>
+      <Outlet />
+    </Page>
+  );
+};
 
 const App = () => {
-  return (
-    <Page>
-      <RouterProvider router={routes} />
-    </Page>
-  )
-}
+  const routesWithLayout = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/chat",
+          element: <Chat />,
+        },
+      ],
+    },
+  ]);
 
-export default App
+  return <RouterProvider router={routesWithLayout} />;
+};
+
+export default App;
