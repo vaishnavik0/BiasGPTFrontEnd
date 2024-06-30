e1dee8ac1dbee2c67cb0b1bf580c155a738b1471
ArthurSenpai <arthursenpai@ArthurSenpais-MacBook-Pro.local>
2024-06-30 23:46:22 +0530

    added meta tags and favicon

--- src/wrappers/Home/Header.tsx
+++ src/wrappers/Home/Header.tsx
@@ -1,9 +1,9 @@
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center flex-col w-[80vw] gap-10">
+    <div className="flex justify-center items-center flex-col w-[80vw] gap-5">
       <h2 className="text-6xl font-extrabold">BiasGPT</h2>
-      <p className="text-center text-2xl min-w-80">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iste mollitia commodi id, tenetur, ipsam velit porro exercitationem quas corrupti dolorem ea ducimus eum excepturi. Rerum velit mollitia aspernatur a.</p>
+      <p className="text-center text-2xl min-w-80">Explore how subtle shifts in language can change perceptions, and learn to create more balanced and inclusive content. Experience the power of perspective with BiasGPT!</p>
     </div>
   );
 };
